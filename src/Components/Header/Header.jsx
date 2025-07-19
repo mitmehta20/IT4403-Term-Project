af74cdca8a62e1e448d00cefcf6fd335a426a7fd
Mit Mehta <mitrmehta20@gmail.com>
2025-07-19 04:25:04 -0400

    Change Page Title

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = ( { parentCallback } ) => {
   return (
     <div className='full-header-container'>
         <div className='header-items'>
-            <div className='header-title'>IT4403 Term Project</div>
+            <div className='header-title'>Coinlore Crypto Platform Data</div>
             <div className='header-navigation'>
                 <div className='nav-item'><button value='global' onClick={onHeaderClick}>Global Stats</button></div>
                 <div className='nav-item'><button value='crypto' onClick={onHeaderClick}>View Crypto</button></div>
